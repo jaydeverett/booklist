6dd434d96072d7498bb6aba767eaac09dde39978
Jay Deverett <jayjdeverett@gmail.com>
2017-08-09 14:48:55 -0400

    add text.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.all
 
+    # raise
+    @books = Book.all
+    #
     respond_to do |format|
      format.html
+     format.text
     end
 
   end
